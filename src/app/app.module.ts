4e74d4db594c6eb879c4e2eaafabb42780d184e1
lulu <lulu9091@gmail.com>
2018-04-06 18:18:21 -0500

    Add three components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FullLayoutComponent } from './full-layout/full-layout.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ResFilterComponent } from './res-filter/res-filter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FullLayoutComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    HeaderComponent,
+    SearchComponent,
+    ItemListComponent,
+    ResFilterComponent
   ],
   imports: [
     BrowserModule,
